f5008b9b3ed28ec68054f25dac2e1ff3eaaac061
Brandon Davis <bdavis@redspin.net>
2017-04-10 17:42:57 -0400

    Working server..

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
-var express = require('express')
-var app = express()
+var express = require('express'),
+    extractor = require('unfluff'),
+    request = require('request'),
+    http = require('http'),
+    url = require('url'),
+    app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('Unfluff-server')
+})
+
+app.get('/unfluff', function (req, res){
+  var unfluff_url = req.query.url; 
+  request(unfluff_url, (error, response, html)=>{
+    if (!error){
+      var data = extractor(html);
+      res.send(data);
+    } else {
+      res.send({
+        error: error
+      });
+    }
+  });
 })
 
 app.listen(3000, function () {
