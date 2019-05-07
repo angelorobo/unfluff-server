b843e33f6bdc559eac3f454a1cf53ace798d64c8
Angelo@Robo <angelo@robovoice.ai>
2019-05-06 18:27:03 -0700

    Remove mongo

--- app.js
+++ app.js
@@ -3,13 +3,10 @@ var express = require('express'),
     request = require('request'),
     http = require('http'),
     url = require('url'),
-    mongo = require('mongodb'),
     test = require('assert'),
     app = express();
 
 const port = 3000;
-const db_name = "reddit";
-const mongo_url = "mongodb://mongo:27017/" + db_name;
 
 
 app.use(function(req, res, next) {
@@ -19,36 +16,9 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.use(function(req, res, next) {
-  // Add the db to the router.
-  mongo.connect(mongo_url, function(err, db) {
-    req.db = db;
-    next();
-  });
-});
-
-/*
-  Database Functions
-*/
-
-var find_url = function(db, url, callback){
-  // If the url is in the database, return whatever json blob you get.
-  var collection = db.collection('unfluff');
-  collection.findOne({url:url}, {}, (err, doc)=>{
-    callback(doc);
-  });
-}
-var insert_url = function(db, url, blob, callback){
-  var collection = db.collection('unfluff');
-  collection.insertOne({
-    url: url, 
-    blob: blob
-  }, {}, callback);
-}
 /*
   Routes
 */
-
 app.get('/', function (req, res) {
   res.send('Unfluff-server')
 });
@@ -56,34 +26,10 @@ app.get('/', function (req, res) {
 app.get('/unfluff', function (req, res){
   var unfluff_url = req.query.url; 
 
-  find_url(req.db, unfluff_url, (result)=>{
-    if (result != null){
-      res.json(result.blob);
-    } else {
-      request(unfluff_url, (error, response, html)=>{
-        if (!error){
-          var data = extractor(html);
-          insert_url(req.db, unfluff_url, data, ()=>{
-            console.log("Inserted " + unfluff_url);
-          });
-          res.json(data);
-        } else {
-          res.json({
-            error: error
-          });
-        }
-      });
-    }
-  });
-});
-
-app.get('/twitter', function (req, res) {
-  var twitter_url = req.query.url;
-  var oEmbed_url = "https://publish.twitter.com/oembed?url=";
-  request(oEmbed_url + twitter_url, (error, response, jason)=>{
+  request(unfluff_url, (error, response, html)=>{
     if (!error){
-      var parsed = JSON.parse(jason);
-      res.json(parsed);
+      var data = extractor(html);
+      res.json(data);
     } else {
       res.json({
         error: error
@@ -93,5 +39,5 @@ app.get('/twitter', function (req, res) {
 });
 
 app.listen(port, function () {
-  console.log('redditAccessible ufluff listening on ' + port);
+  console.log('ufluff listening on ' + port);
 });
---
+++ run.sh
@@ -0,0 +1,1 @@
+docker run -d --name unfluff -p 3000:3000 unfluff
